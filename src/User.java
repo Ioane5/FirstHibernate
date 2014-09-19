554fa10357fb279c9d6e74e2660e317d8c74a50b
ioane5 <ioane.sharvadze@gmail.com>
2014-09-20 00:41:42 +0400

    Hibernate
    Hibernate Project jars added.
    also added sample class

---
+++ src/User.java
@@ -0,0 +1,64 @@
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	private int id;
+
+	private String firstName;
+
+	private String lastName;
+
+	private Date birthDate;
+
+	private String telephoneNumber;
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getTelephoneNumber() {
+		return telephoneNumber;
+	}
+
+	public void setTelephoneNumber(String telephoneNumber) {
+		this.telephoneNumber = telephoneNumber;
+	}
+
+	//private List<User> friends;
+
+
+}
